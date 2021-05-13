bde0e4be6ca81e05cbd5f13cb9e873c9220b3d29
FrancescoVisco <francevisco@gmail.com>
2021-05-13 17:44:53 +0200

    Test Fix End Level

--- Assets/Scripts/Menu/LevelTrigger.cs
+++ Assets/Scripts/Menu/LevelTrigger.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class LevelTrigger : MonoBehaviour
 {
-    private int ThisScene;
+    public int ThisScene;
+    private bool InTrigger = false;
     
     void Start()
     {
@@ -14,15 +15,19 @@ public class LevelTrigger : MonoBehaviour
 
     void Update()
     {
-        
+        if(InTrigger == true)
+        {
+            GameObject.FindGameObjectWithTag("Player").GetComponent<FirstPersonAIO>().enabled = false;
+            GameObject.FindGameObjectWithTag("Player").GetComponent<SpawnWaypoint>().enabled = false;
+            GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>().isKinematic = true;
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
-            GameObject.Find("Player").GetComponent<FirstPersonAIO>().enabled = false;
-            GameObject.Find("Player").GetComponent<SpawnWaypoint>().enabled = false;
+            InTrigger = true;
             GameObject.Find("LevelLoader").GetComponent<LevelLoader>().Fade = true;
             GameObject.Find("LevelLoader").GetComponent<LevelLoader>().transitionTime = 2f;
             GameObject.Find("LevelLoader").GetComponent<LevelLoader>().SceneToLoad = ThisScene+1;
